95ad446c60dfd22ee0fa724ee1ccf0316d58207b
Anselm Levskaya <levskaya@google.com>
2024-03-04 20:41:25 -0800

    tweak pipelining animation

--- pallas_pipelining/pipelining.js
+++ pallas_pipelining/pipelining.js
@@ -4,7 +4,7 @@ import { CSS2DRenderer } from '/external/three/CSS2DRenderer.js';
 import { capture_and_control_ui } from '/lib/control_ui.js';
 import { Rect, Text, Label } from '/lib/boxpusher.js';
 import { map, empty} from '/lib/nd.js';
-import { v3 } from '/lib/vectors.js';
+import { v3, add } from '/lib/vectors.js';
 import * as colors from '/lib/colors.js';
 
 // debug
@@ -59,31 +59,23 @@ const neg_half_dx = dx.clone().multiplyScalar(-0.5);
 const ahead_dx = dx.clone().multiplyScalar(N + 0.5)
 
 // HBM coordinates
-const x_hbm = v3(70.0, 610.0, 0.0);
-const y_hbm = v3(200.0, 610.0, 0.0);
-const z_hbm = v3(330.0, 610.0, 0.0);
-const x0_hbm = x_hbm.clone().add(neg_half_dx);
-const y0_hbm = y_hbm.clone().add(neg_half_dx);
-const z0_hbm = z_hbm.clone().add(neg_half_dx);
-const x1_hbm = x_hbm.clone().add(ahead_dx);
-const y1_hbm = y_hbm.clone().add(ahead_dx);
-const z1_hbm = z_hbm.clone().add(ahead_dx);
+const x_hbm = v3(70, 610, 0);
+const y_hbm = add(x_hbm, v3(130, 0, 0));
+const z_hbm = add(y_hbm, v3(130, 0, 0));
+const [x0_hbm, y0_hbm, z0_hbm] = [x_hbm, y_hbm, z_hbm].map(x => add(x, neg_half_dx));
+const [x1_hbm, y1_hbm, z1_hbm] = [x_hbm, y_hbm, z_hbm].map(x => add(x, ahead_dx));
 
 // VMEM coordinates
-const x_vmem = v3(275.0, 360.0, 0.0);
-const y_vmem = v3(390.0, 360.0, 0.0);
-const z_vmem = v3(505.0, 360.0, 0.0);
-const x0_vmem = x_vmem.clone().add(neg_half_dx);
-const y0_vmem = y_vmem.clone().add(neg_half_dx);
-const z0_vmem = z_vmem.clone().add(neg_half_dx);
-const x1_vmem = x_vmem.clone().add(ahead_dx);
-const y1_vmem = y_vmem.clone().add(ahead_dx);
-const z1_vmem = z_vmem.clone().add(ahead_dx);
+const x_vmem = v3(275, 360, 0);
+const y_vmem = add(x_vmem, v3(115, 0, 0));
+const z_vmem = add(y_vmem, v3(115, 0, 0));
+const [x0_vmem, y0_vmem, z0_vmem] = [x_vmem, y_vmem, z_vmem].map(x => add(x, neg_half_dx));
+const [x1_vmem, y1_vmem, z1_vmem] = [x_vmem, y_vmem, z_vmem].map(x => add(x, ahead_dx));
 
 // VREG coordinates
-const x_vreg = v3(350.0, 240.0, 0.0);
-const y_vreg = v3(400.0, 240.0, 0.0);
-const z_vreg = v3(450.0, 240.0, 0.0);
+const x_vreg = v3(350, 240, 0);
+const y_vreg = add(x_vreg, v3(50, 0, 0));
+const z_vreg = add(y_vreg, v3(50, 0, 0));
 
 // Vector Core coordinates
 const vector_core = v3(410.0, 110.0, 0.0);
@@ -136,11 +128,10 @@ let X0s = make_boxes(x0_hbm, colors.red);
 let X1s = make_boxes(x1_hbm, colors.red);
 let Y0s = make_boxes(y0_hbm, colors.green);
 let Y1s = make_boxes(y1_hbm, colors.green);
-
 let Z0s = make_boxes(vector_core, colors.blue, 0.0);
 let Z1s = make_boxes(vector_core, colors.blue, 0.0);
 
-// grab an element from background SVG
+// grab an element from the background SVG
 let svg_timeline = gsap.timeline();
 let svgroot = document.getElementById('background').contentDocument;
 const created_buffers_in_svg = Array.from(svgroot.querySelectorAll('.st5,.st6'));
@@ -160,15 +151,14 @@ created_buffers_in_svg.forEach(element => {
 });
 
 let t1 = push_packets(X0s, x0_hbm, x0_vmem, t0, hbm_tick);
-let t2 = push_packets(Y0s, y0_hbm, y0_vmem, t1, hbm_tick);
+let t2 = push_packets(Y0s, y0_hbm, y0_vmem, t0 + hbm_tick*N, hbm_tick);
 let t3 = push_packets(X1s, x1_hbm, x1_vmem, t2, hbm_tick);
-let t4 = push_packets(Y1s, y1_hbm, y1_vmem, t3, hbm_tick);
+let t4 = push_packets(Y1s, y1_hbm, y1_vmem, t2 + hbm_tick*N, hbm_tick);
 
-let t5 = push_packets(X0s, x0_vmem, x_vreg, t2, vmem_tick);
-let t6 = push_packets(Y0s, y0_vmem, y_vreg, t5, vmem_tick);
-
-let t7 = push_packets(X0s, x_vreg, vector_core, t6, vpu_tick);
-         push_packets(Y0s, y_vreg, vector_core, t6, vpu_tick);
+let t5 = push_packets(X0s, x0_vmem, x_vreg, t2 + 3*hbm_tick, vmem_tick);
+let t6 = push_packets(Y0s, y0_vmem, y_vreg, t2 + 3*hbm_tick, vmem_tick);
+let t7 = push_packets(X0s, x_vreg, vector_core, t2 + 6*hbm_tick, vpu_tick);
+         push_packets(Y0s, y_vreg, vector_core, t2 + 6*hbm_tick, vpu_tick);
 
 // hide green/red, reveal blue
 map((b) => b.toOpacity(0.0, t7, tick*0.1), X0s);
@@ -176,11 +166,11 @@ map((b) => b.toOpacity(0.0, t7, tick*0.1), Y0s);
 map((b) => b.toOpacity(1.0, t7, tick*0.1), Z0s);
 
 let t8 = push_packets(Z0s, vector_core, z_vreg, t7, vpu_tick);
-let t9 = push_packets(Z0s, z_vreg, z0_vmem, t8, vpu_tick);
-let t10 = push_packets(Z0s, z0_vmem, z0_hbm, Math.max(t9, t4), hbm_tick);
+let t9 = push_packets(Z0s, z_vreg, z0_vmem, t8, vmem_tick);
+let t10 = push_packets(Z0s, z0_vmem, z0_hbm, t9, hbm_tick);
 
-let t11 = push_packets(X1s, x1_vmem, x_vreg, Math.max(t4, t9), vmem_tick);
-let t12 = push_packets(Y1s, y1_vmem, y_vreg, Math.max(t11, t4 + 4*hbm_tick), vmem_tick);
+let t11 = push_packets(X1s, x1_vmem, x_vreg, t4 + 3*hbm_tick, vmem_tick);
+let t12 = push_packets(Y1s, y1_vmem, y_vreg, t4 + 3*hbm_tick, vmem_tick);
 let t13 = push_packets(X1s, x_vreg, vector_core, t12, vpu_tick);
           push_packets(Y1s, y_vreg, vector_core, t12, vpu_tick);
 
@@ -190,8 +180,8 @@ map((b) => b.toOpacity(0.0, t13, tick*0.1), Y1s);
 map((b) => b.toOpacity(1.0, t13, tick*0.1), Z1s);
 
 let t14 = push_packets(Z1s, vector_core, z_vreg, t13, vpu_tick);
-let t15 = push_packets(Z1s, z_vreg, z1_vmem, t14, vpu_tick);
-let t16 = push_packets(Z1s, z1_vmem, z1_hbm, Math.max(t15, t10), hbm_tick);
+let t15 = push_packets(Z1s, z_vreg, z1_vmem, t14, vmem_tick);
+let t16 = push_packets(Z1s, z1_vmem, z1_hbm, t15, hbm_tick);
 
 // duration of root gsap timeline (total length of scheduled animations)
 let finalTime = gsap.globalTimeline.endTime();
